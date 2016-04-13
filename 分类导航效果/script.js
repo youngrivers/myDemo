02c8130ae71f0c9b25189c48e82ac0fd4a7ecabf
youngrivers <401712228@qq.com>
2016-04-14 00:01:30 +0800

    分类导航效果0414

--- 分类导航效果/script.js
+++ 分类导航效果/script.js
@@ -1,3 +1,14 @@
 /**
  * Created by youngrivers on 2016/4/13.
  */
+window.onload = function () {
+    var oLi=document.getElementsByTagName("li");
+    for(var i= 0;i<oLi.length;i++){
+        oLi[i].onmouseover=function(){
+            this.className="lihover";
+        };
+        oLi[i].onmouseout=function(){
+            this.className="";
+        }
+    }
+};
