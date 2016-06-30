a3d87baebddec6971f536c16097e9bb5bdd285f2
youngrivers <401712228@qq.com>
2016-06-30 23:36:47 +0800

    新增demo

--- 生成随机数/script.js
+++ 生成随机数/script.js
@@ -4,8 +4,15 @@
 var text1=document.getElementById("text1");
 var text2=document.getElementById("text2");
 var text3=document.getElementById("text3");
-var btn=document.getElementById("btn");
+var text4=document.getElementById("text4");
+var btn1=document.getElementById("btn1");
+var btn2=document.getElementById("btn2");
 var box=document.getElementById("box");
+var redBall=document.getElementById("redBall");
+var blueBall=document.getElementById("blueBall");
+var Ball=document.getElementById("ball");
+var oLi=document.getElementsByTagName("li");
+console.log(oLi.length);
 function oNum(count,max){
     var num=[];
     if(text2.value==0){
@@ -15,6 +22,37 @@ function oNum(count,max){
     }
     return num;
 }
-btn.onclick=function(){
+btn1.onclick=function(){
     box.innerHTML=oNum(text1.value,text3.value);
 };
+//双色球
+btn2.onclick=function(){
+    Ball.innerText="红球："+red()+"蓝球："+blue();
+    if(text4.value>1){
+        for(var i=0;i<text4.value;i++){
+            oLi[i].innerText="红球："+red()+"蓝球："+blue();
+        }
+    }
+};
+function blue(){
+    var blueCount=[];
+    for(var i=0;i<1;i++){
+        blueCount[i]=Math.round(Math.random()*15+1);
+    }
+    return blueCount;
+}
+function red(){
+    var redCount=[];
+    for(var i=0;i<6;i++){
+        redCount[i]=Math.round(Math.random()*32+1);
+        //判断是否重复
+        if(i>0){
+            for(var j=0;j<i;j++){
+                if(redCount[i]==redCount[j]){
+                    redCount[i]=Math.round(Math.random()*32+1);
+                }
+            }
+        }
+    }
+    return redCount;
+}
