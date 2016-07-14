344bb9956fa96bf3dabd1e068eec157c2931b6fd
youngrivers <401712228@qq.com>
2016-07-15 04:44:15 +0800

    Offcanvas实现

---
+++ 基于Bootstrap的网页/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by youngrivers on 2016/7/15.
+ */
+$(document).ready(function () {
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+});
